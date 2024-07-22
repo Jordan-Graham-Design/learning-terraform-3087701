4c530b49126ba2e65c420e81d65a33864aa4f242
Jordan-Graham-Design <48306741+Jordan-Graham-Design@users.noreply.github.com>
2024-07-22 14:29:10 -0700

    syntax fixed

--- main.tf
+++ main.tf
@@ -25,7 +25,7 @@ module "blog_vpc" {
   name                                = "dev"
   azs                                 = ["us-west-2a", "us-west-2b", "us-west-2c"]
 
-  public subnets                      = ["10.0.101.0/24", "10.0.102.0/24","10.0.103.0/24"]
+  public_subnets                      = ["10.0.101.0/24", "10.0.102.0/24","10.0.103.0/24"]
 
   tags = {
     Terraform   = "true"
