3fb5392bb6bcdee22cb5606a686b2c22ac7ffd9f
Jordan-Graham-Design <48306741+Jordan-Graham-Design@users.noreply.github.com>
2024-07-22 14:34:16 -0700

    adjusted changes to match video for awc vpc source and cidr

--- main.tf
+++ main.tf
@@ -19,10 +19,11 @@ data "aws_vpc" "default" {
 }
 
 module "blog_vpc" {
-  source                              = "clouddrove/vpc/aws"
+  source = "terraform-aws-modules/vpc/aws"
     
-  version                             = "2.0.0"
-  name                                = "dev"
+  name = "dev"
+  cidr = "10.0.0.0/16"
+  
   azs                                 = ["us-west-2a", "us-west-2b", "us-west-2c"]
 
   public_subnets                      = ["10.0.101.0/24", "10.0.102.0/24","10.0.103.0/24"]
